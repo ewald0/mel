3445d41adaad62adf3eed2c5c02e893399415bf7
ewaldo <eduardoverag@hotmail.com>
2019-11-20 03:11:01 -0400

    sitio web de melanie

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    
+
+    $('.js--section-cualidades').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+});
+
